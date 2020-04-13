88faca322ccb4cf80862dda01b524fc53a7de055
Bogdan Prăzniciuc <bogdan@onis.studio>
2020-04-13 13:56:44 +0100

    Added connection to MongoDB

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'personal_journal'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello Journal'
+@app.route('/get_stories')
+def get_stories():
+    return render_template("stories.html", stories=mongo.db.stories.find())
 
 
 if __name__ == '__main__':
